8e818a2060860cbf22802a0d5bf83699be58d0bc
Hamed Saadati <hamedsaadati1384@yahoo.com>
2024-06-27 00:48:34 +0330

    commited

---
+++ cmake-build-debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_amin_dopingi_game_FILE C:/Users/Hamed/Desktop/amin dopingi game/cmake-build-debug/amin_dopingi_game.exe)
+set(__QT_DEPLOY_TARGET_amin_dopingi_game_RUNTIME_DLLS C:/Qt/6.7.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.1/mingw_64/bin/Qt6Core.dll)
