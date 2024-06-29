ca287512b29d900bda4dbc9606df4da158c3340b
Hamed Saadati <hamedsaadati1384@yahoo.com>
2024-06-29 23:30:24 +0330

    create game

---
+++ classes/Game.h
@@ -0,0 +1,18 @@
+
+
+#ifndef AMIN_DOPINGI_GAME_GAME_H
+#define AMIN_DOPINGI_GAME_GAME_H
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsPixmapItem>
+
+class Game:public QGraphicsScene,public QGraphicsView {
+
+    public:
+    Game();
+
+
+};
+
+
+#endif //AMIN_DOPINGI_GAME_GAME_H
