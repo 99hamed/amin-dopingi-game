701c5d675e07c3fde443792e5269f66f74e39583
Hamed Saadati <hamedsaadati1384@yahoo.com>
2024-06-30 18:06:12 +0330

    added background and hill in game

--- classes/Game.cpp
+++ classes/Game.cpp
@@ -8,9 +8,15 @@ Game::Game(){
 
     scene->setSceneRect(0,0,QWidget::width(),QWidget::height());
 
-   scene->setBackgroundBrush(QBrush(QImage(":/images/background.png")));
-//    auto background= new Decorator();
-//    scene->addItem(background);
+   scene->setBackgroundBrush(QBrush(QColor("#007786")));
+
+
+    auto background= new BackgroundDecorator(QWidget::width(),QWidget::height());
+    auto background2= new HillDecorator(QWidget::width(),QWidget::height());
+    scene->addItem(background);
+    scene->addItem(background2);
+    background->setPos(10,10);
+    background2->setPos(10,10);
     setScene(scene);
 
 }
