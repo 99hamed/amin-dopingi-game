d346545d9cc6fde530cbdb01a4f82c846d173610
Hamed Saadati <hamedsaadati1384@yahoo.com>
2024-07-02 02:03:17 +0330

    show test of player animation

--- classes/Player.cpp
+++ classes/Player.cpp
@@ -1,12 +1,42 @@
 
-
 #include "Player.h"
+
 Player::Player(int width, int height, Position position, int speed, Position velocity, QGraphicsPixmapItem *image)
         : BodyObject(width, height, position, image), speed(speed), velocity(velocity) {}
 
+
+Player::Player(int width,int height,QGraphicsItem* parent): QGraphicsPixmapItem(parent)
+        {
+
+    auto pixmap =new QPixmap (":/resources/images/spriteStandRight.png");
+    auto scaledpixmap=pixmap->scaled(89.5,196.5);
+
+    setPixmap(scaledpixmap);
+
+    frames.append(new QPixmap(scaledpixmap));
+    pixmap=new QPixmap(":/resources/images/spriteRunRight.png");
+    scaledpixmap=pixmap->scaled(5000,200);
+
+    scaledpixmap = scaledpixmap.copy(0, 0, pixmap->width() - 10050 , pixmap->height());
+    frames.append(new QPixmap(scaledpixmap));
+
+    runningtimer= new QTimer();
+    runningtimer->setInterval(300);
+    connect(runningtimer,&QTimer::timeout,this,&Player::handleRightMovement);
+    runningtimer->start();
+
+    setPos(100,height/2-pixmap->height()/2);
+
+
+}
 void Player::draw(QGraphicsScene &scene)  {
     if (getimage()) {
         getimage()->setPos(getpos().getx(), getpos().gety());
         scene.addItem(getimage());
     }
+}
+
+void Player::handleRightMovement(){
+    setPixmap(*frames.at(frame));
+    frame=(frame+1)%2;
 }
--- classes/Player.h
+++ classes/Player.h
@@ -3,18 +3,30 @@
 #ifndef AMIN_DOPINGI_GAME_PLAYER_H
 #define AMIN_DOPINGI_GAME_PLAYER_H
 #include"BodyObject.h"
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QGraphicsPixmapItem>
+#include<QList>
+#include<QTimer>
+#include<QObject>
 
-    class Player : public BodyObject {
-    public:
+
+    class Player :public QObject,public BodyObject,public QGraphicsPixmapItem {
+        Q_OBJECT
+        private:
+        int frame{};
+        QList<QPixmap*> frames{};
+        QTimer* runningtimer;
+        public:
         int speed;
         Position velocity;
 
         Player(int,int ,Position ,int,Position, QGraphicsPixmapItem *image = nullptr);
+        Player(int ,int,QGraphicsItem* parent= nullptr);
+
+
 
         void handleGravity();
-
-        void handleRightMovement();
-
 
         void handleLeftMovement();
 
@@ -28,7 +40,8 @@
 
 
         void draw(QGraphicsScene &scene) override;
-
+public slots:
+        void handleRightMovement();
 
 
 };
