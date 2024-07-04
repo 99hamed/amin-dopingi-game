7e7e6eca8c2bf0336c239e27b4c8cdb1c20b907f
Hamed Saadati <hamedsaadati1384@yahoo.com>
2024-07-04 20:17:30 +0330

    udated version

---
+++ cmake-build-debug/amin_dopingi_game_autogen/QESDO5J2AP/moc_Player.cpp
@@ -0,0 +1,171 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'Player.h'
+**
+** Created by: The Qt Meta Object Compiler version 68 (Qt 6.7.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../../classes/Player.h"
+#include <QtCore/qmetatype.h>
+
+#include <QtCore/qtmochelpers.h>
+
+#include <memory>
+
+
+#include <QtCore/qxptype_traits.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'Player.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 68
+#error "This file was generated using the moc from 6.7.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+#ifndef Q_CONSTINIT
+#define Q_CONSTINIT
+#endif
+
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+QT_WARNING_DISABLE_GCC("-Wuseless-cast")
+namespace {
+
+#ifdef QT_MOC_HAS_STRINGDATA
+struct qt_meta_stringdata_CLASSPlayerENDCLASS_t {};
+constexpr auto qt_meta_stringdata_CLASSPlayerENDCLASS = QtMocHelpers::stringData(
+    "Player",
+    "handleGravity",
+    "",
+    "Running",
+    "width",
+    "height"
+);
+#else  // !QT_MOC_HAS_STRINGDATA
+#error "qtmochelpers.h not found or too old."
+#endif // !QT_MOC_HAS_STRINGDATA
+} // unnamed namespace
+
+Q_CONSTINIT static const uint qt_meta_data_CLASSPlayerENDCLASS[] = {
+
+ // content:
+      12,       // revision
+       0,       // classname
+       0,    0, // classinfo
+       2,   14, // methods
+       2,   28, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags, initial metatype offsets
+       1,    0,   26,    2, 0x0a,    3 /* Public */,
+       3,    0,   27,    2, 0x0a,    4 /* Public */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+
+ // properties: name, type, flags
+       4, QMetaType::QReal, 0x00015003, uint(-1), 0,
+       5, QMetaType::QReal, 0x00015003, uint(-1), 0,
+
+       0        // eod
+};
+
+Q_CONSTINIT const QMetaObject Player::staticMetaObject = { {
+    QMetaObject::SuperData::link<QObject::staticMetaObject>(),
+    qt_meta_stringdata_CLASSPlayerENDCLASS.offsetsAndSizes,
+    qt_meta_data_CLASSPlayerENDCLASS,
+    qt_static_metacall,
+    nullptr,
+    qt_incomplete_metaTypeArray<qt_meta_stringdata_CLASSPlayerENDCLASS_t,
+        // property 'width'
+        QtPrivate::TypeAndForceComplete<qreal, std::true_type>,
+        // property 'height'
+        QtPrivate::TypeAndForceComplete<qreal, std::true_type>,
+        // Q_OBJECT / Q_GADGET
+        QtPrivate::TypeAndForceComplete<Player, std::true_type>,
+        // method 'handleGravity'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>,
+        // method 'Running'
+        QtPrivate::TypeAndForceComplete<void, std::false_type>
+    >,
+    nullptr
+} };
+
+void Player::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<Player *>(_o);
+        (void)_t;
+        switch (_id) {
+        case 0: _t->handleGravity(); break;
+        case 1: _t->Running(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::ReadProperty) {
+        auto *_t = static_cast<Player *>(_o);
+        (void)_t;
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: *reinterpret_cast< qreal*>(_v) = _t->x(); break;
+        case 1: *reinterpret_cast< qreal*>(_v) = _t->y(); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::WriteProperty) {
+        auto *_t = static_cast<Player *>(_o);
+        (void)_t;
+        void *_v = _a[0];
+        switch (_id) {
+        case 0: _t->setX(*reinterpret_cast< qreal*>(_v)); break;
+        case 1: _t->setY(*reinterpret_cast< qreal*>(_v)); break;
+        default: break;
+        }
+    } else if (_c == QMetaObject::ResetProperty) {
+    } else if (_c == QMetaObject::BindableProperty) {
+    }
+    (void)_a;
+}
+
+const QMetaObject *Player::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *Player::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_CLASSPlayerENDCLASS.stringdata0))
+        return static_cast<void*>(this);
+    if (!strcmp(_clname, "BodyObject"))
+        return static_cast< BodyObject*>(this);
+    if (!strcmp(_clname, "QGraphicsPixmapItem"))
+        return static_cast< QGraphicsPixmapItem*>(this);
+    return QObject::qt_metacast(_clname);
+}
+
+int Player::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QObject::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 2)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 2)
+            *reinterpret_cast<QMetaType *>(_a[0]) = QMetaType();
+        _id -= 2;
+    }else if (_c == QMetaObject::ReadProperty || _c == QMetaObject::WriteProperty
+            || _c == QMetaObject::ResetProperty || _c == QMetaObject::BindableProperty
+            || _c == QMetaObject::RegisterPropertyMetaType) {
+        qt_static_metacall(this, _c, _id, _a);
+        _id -= 2;
+    }
+    return _id;
+}
+QT_WARNING_POP
