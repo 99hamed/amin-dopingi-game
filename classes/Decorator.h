5c06268faaf40ecd769fe74a577f6a9d85a6cd4c
Mohamad hosein Mirmajidi <m.h.mirmajidi8495@gmail.com>
2024-07-05 16:35:13 +0330

    update decorator

--- classes/Decorator.h
+++ classes/Decorator.h
@@ -1,35 +1,33 @@
-
 #ifndef AMIN_DOPINGI_GAME_DECORATOR_H
 #define AMIN_DOPINGI_GAME_DECORATOR_H
-#include"BodyObject.h"
+#include "BodyObject.h"
 #include <QGraphicsScene>
 #include <QGraphicsView>
 #include <QGraphicsPixmapItem>
 
-class Decorator : public BodyObject,public QGraphicsPixmapItem {
+class Decorator : public BodyObject, public QGraphicsPixmapItem {
 public:
-    Decorator(int,int,Position, QGraphicsPixmapItem *image = nullptr);
-
-    Decorator(int,int,QGraphicsItem* parent =nullptr);
-
+    Decorator(int width, int height, Position position, QGraphicsPixmapItem *image = nullptr);
+    Decorator(int width, int height, QGraphicsItem *parent = nullptr);
 };
 
 class BackgroundDecorator : public Decorator {
 public:
-    BackgroundDecorator(int, int , Position , QGraphicsPixmapItem *image = nullptr);
-    BackgroundDecorator(int,int ,QGraphicsItem* parent =nullptr);
+    BackgroundDecorator(int width, int height, Position position, QGraphicsPixmapItem *image = nullptr);
+    BackgroundDecorator(int width, int height, QGraphicsItem *parent = nullptr);
 
     void draw(QGraphicsScene &scene) override;
-
+    void updatePosition(int x, int y);
+    void setScale(float scale);
 };
 
 class HillDecorator : public Decorator {
 public:
-    HillDecorator(int, int , Position , QGraphicsPixmapItem *image );
-
-    HillDecorator(int, int, QGraphicsItem *parent = nullptr);
+    HillDecorator(int width, int height, Position position, QGraphicsPixmapItem *image);
+    HillDecorator(int width, int height, QGraphicsItem *parent = nullptr);
 
     void draw(QGraphicsScene &scene) override;
-
+    void updatePosition(int x, int y);
 };
+
 #endif //AMIN_DOPINGI_GAME_DECORATOR_H
