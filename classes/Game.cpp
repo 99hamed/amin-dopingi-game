c97083e5275b5b84b04c0841c883b8a5d34f6708
Hamed Saadati <hamedsaadati1384@yahoo.com>
2024-07-07 22:44:17 +0330

    update game

--- classes/Game.cpp
+++ classes/Game.cpp
@@ -1,61 +1,66 @@
 
 #include "Game.h"
 
-
 Game::Game() {
     showFullScreen();
+
     setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
-    auto scene = new QGraphicsScene();
+    scene = new QGraphicsScene();
     setScene(scene);
     scene->setSceneRect(0, 0, QWidget::width(), QWidget::height());
-
     scene->setBackgroundBrush(QBrush(QColor("#007786")));
-//    auto playground = new QGraphicsView();
-//
-//    playground->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-//    playground->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-//    playground->setFrameShape(QFrame::NoFrame);
-//
-////    playgroundScene = new QGraphicsScene(this);
-////    playground->setScene(playgroundScene);
 
-
-    auto background = new BackgroundDecorator(QWidget::width(),QWidget::height());
-    auto background2 = new HillDecorator(QWidget::width(),QWidget::height());
+    background = new BackgroundDecorator(QWidget::width(), QWidget::height());
+    background2 = new HillDecorator(QWidget::width(), QWidget::height());
     scene->addItem(background);
     scene->addItem(background2);
-    auto player = new Player( QWidget::width(),QWidget::height());
+
+    player = new Player(QWidget::width(), QWidget::height());
+    player->speed = 30; // player speed
     scene->addItem(player);
 
-//
-//    QPixmap platformPixmap(":/resources/images/platform.png");
-//    auto platform = new Platform(platformPixmap);
-//    scene->addItem(platform);
-//    platform->setPos(0, (scene->height()/2)+100);
-//
-//
-//
-//    object = new QGraphicsPixmapItem(QPixmap(":/resources/images/platform.png"));
-//    object->setPos(579, (scene->height()/2)+100);
-//    scene->addItem(object);
+           QPixmap platformPixmap(":/resources/images/platform.png");
+            platform = new Platform(platformPixmap);
+            platform->setPos(500,QWidget::height()-125);
+            scene->addItem(platform);
+
+
+
 
     setScene(scene);
-    // Setup the view
-//    view = new QGraphicsView(scene);
-//    view->setFixedSize(800, 600);
-//    view->show();
-    //setScene(scene);
-    // Setup the timer
-//    timer = new QTimer();
-//    connect(timer, &QTimer::timeout, this, &Game::update);
-//    timer->start(16); // Approx. 60 FPS
-//
-//   PlatformGeneratorTimer = new QTimer();
+
+    timer = new QTimer(this);
+    connect(timer, &QTimer::timeout, this, &Game::updateScene);
+    timer->start(16); // 60 FPS
+
+    //   PlatformGeneratorTimer = new QTimer();
 //    PlatformGeneratorTimer->setInterval(2000);
 //   connect(PlatformGeneratorTimer, &QTimer::timeout, this, &Game::addPlatform);
 //  PlatformGeneratorTimer->start();
 
 }
 
+void Game::updateScene() {
+    int middleX = scene->width() / 2;
+    int playerX = player->x();
+
+    int dx = 0;
+    if (playerX > middleX) {
+        dx = playerX - middleX;
+    } else if (playerX < middleX && scene->sceneRect().x() > 0) {
+        dx = playerX - middleX;
+    }
+
+    if (dx != 0) {
+        int backgroundSpeed = dx / 4; // scroll speed
+
+        scene->setSceneRect(scene->sceneRect().x() + dx, 0, scene->width(), scene->height());
+        background->updatePosition(background2->x() - backgroundSpeed, background->y());
+        background2->updatePosition(background2->x() - backgroundSpeed, background2->y());
+
+        // Keep the player at the middle
+        player->setPos(middleX, player->y());
+    }
+}
--- classes/Game.h
+++ classes/Game.h
@@ -1,29 +1,36 @@
 
+
 #ifndef GAME_H
 #define GAME_H
+
 #include <QGraphicsScene>
 #include <QGraphicsView>
 #include <QGraphicsPixmapItem>
 #include <QTimer>
-#include"Decorator.h"
-#include"Player.h"
+#include "Decorator.h"
+#include "Player.h"
+#include "platform.h"
 
-class Game:public QGraphicsView{
+class Game : public QGraphicsView {
 Q_OBJECT
 
-
     QTimer* PlatformGeneratorTimer;
-    QGraphicsScene *scene;
-    QGraphicsView *view;
-    //Platform *platform;
-    QGraphicsPixmapItem *object;
-    QTimer *timer;
-public:
+    QGraphicsScene* scene;
+    QGraphicsView* view;
+    QGraphicsPixmapItem* object;
+    QTimer* timer;
+    Player* player;
+    BackgroundDecorator* background;
+    HillDecorator* background2;
+    Platform *platform;
+
+
 public:
     Game();
 
-
+public slots:
+    void updateScene();
 };
 
+#endif // GAME_H
 
-#endif //AMIN_DOPINGI_GAME_GAME_H
